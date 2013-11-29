3e89e22441e2690234fba25fd5d2cdba1782a599
Uldis Sturms <uldis.sturms@gmail.com>
2013-11-29 18:12:44 +0000

    Added request rate limiting for haproxy

--- cookbooks/haproxy/recipes/default.rb
+++ cookbooks/haproxy/recipes/default.rb
@@ -1,3 +1,23 @@
+package "build-essential" do
+  action :install
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/haproxy-1.5-dev19.tar.gz" do
+  source "http://haproxy.1wt.eu/download/1.5/src/devel/haproxy-1.5-dev19.tar.gz"
+  action :create_if_missing
+end
+
+bash "install_haproxy" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    tar -zvxf haproxy-1.5-dev19
+    cd haproxy-1.5-dev19
+    make TARGET=linux26
+    cp haproxy /usr/sbin/haproxy
+  EOH
+  creates "/usr/sbin/haproxy"
+end
+
 cookbook_file "/etc/default/haproxy" do
     source "haproxy-default"
     owner "root"
@@ -14,8 +34,12 @@ cookbook_file "/etc/haproxy/haproxy.cfg" do
   notifies :restart, "service[haproxy]"
 end
 
-package "haproxy" do
-  action :install
+cookbook_file "/etc/haproxy/whitelist.lst" do
+  source "whitelist.lst"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :restart, "service[haproxy]"
 end
 
 service "haproxy" do
--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -2,7 +2,15 @@ package "nginx" do
   action :install
 end
 
-service 'nginx' do
+cookbook_file "/etc/nginx/sites-enabled/default" do
+  source "default"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :restart, "service[nginx]"
+end
+
+service "nginx" do
     supports :status => true, :restart => true, :reload => true
       action :start
 end
